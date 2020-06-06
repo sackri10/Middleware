a9ca25bf00bca4b0578cca7945fbac34c6410df5
Datta Sai Krishna <sackri10@gmail.com>
2020-06-06 21:43:20 +0530

    merge

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace Middleware
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    
                     webBuilder.UseStartup<Startup>();
                 });
     }
